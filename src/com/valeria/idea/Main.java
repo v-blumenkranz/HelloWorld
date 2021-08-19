513a5bb9996dc186b72f8d5f0ad883e06f4d4ef9
v-blumenkranz <valeriayarovtseva@yandex.ru>
2021-08-19 20:19:32 +0300

    Add double and float variables (Task 2)

--- src/com/valeria/idea/Main.java
+++ src/com/valeria/idea/Main.java
@@ -3,5 +3,7 @@ package com.valeria.idea;
 public class Main {
     public static void main (String[] args){
         System.out.println("Hello world!");
+        double a=12.12;
+        float b=12.12F;
     }
 }
